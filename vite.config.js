4c32de5cb755617c2c3d5c87d9b21e819ad63692
Chadrack Kanza <chadrackkanza2015@gmail.com>
2024-04-08 19:03:54 +0100

    base: "https://chadrackkanza.github.io/vaed",

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vaed",
+  base: "https://chadrackkanza.github.io/vaed",
   plugins: [
     vue(),
     vueJsx(),
